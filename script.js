1890f9f89937b5325cc272d145dd41945af4b183
Amytistshop <bannovsasa266@gmail.com>
2026-01-08 12:07:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// MENU
 const menuBtn = document.getElementById("menuBtn");
 const sideMenu = document.getElementById("sideMenu");
 
@@ -6,9 +7,33 @@ menuBtn.onclick = () => {
     sideMenu.style.right === "0px" ? "-220px" : "0px";
 };
 
+// FAQ
 document.querySelectorAll(".faq-question").forEach(q => {
   q.onclick = () => {
     const ans = q.nextElementSibling;
-    ans.style.display = ans.style.display === "block" ? "none" : "block";
+    const icon = q.querySelector("span");
+
+    if (ans.style.display === "block") {
+      ans.style.display = "none";
+      icon.textContent = "➕";
+    } else {
+      ans.style.display = "block";
+      icon.textContent = "➖";
+    }
   };
 });
+
+// TYPE ANIMATION
+const text = "AmytistShop";
+const target = document.getElementById("typeText");
+let i = 0;
+
+function typeEffect() {
+  if (i < text.length) {
+    target.textContent += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 120);
+  }
+}
+
+typeEffect();
